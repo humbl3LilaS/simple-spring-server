91738cc3d8d02cad1eedb5affafa9d894cdbbd45
humbl3LilaS <sabishinekobebe@gmail.com>
2024-08-09 11:23:53 +0630

    initial setup

---
+++ src/main/java/com/edelweiss/app/SimpleSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.edelweiss.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edelweiss/app/SimpleSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edelweiss.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
